b61ffd08d1fc01f40336c420815379213824dfe8
kostuk42 <kostuk42@i.ua>
2021-04-22 15:27:54 +0300

    Initial commit

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,5 +1,5 @@
 const input = document.querySelector('#font-size-control');
-// let volume = input.value;
+
 const output = document.querySelector('#text');
 input.addEventListener('input', () => {
     let volume = input.value;
