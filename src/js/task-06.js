fac4f34c403c95781574ee000322cdef5c13b758
kostuk42 <kostuk42@i.ua>
2021-05-24 15:51:24 +0300

    upd #6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -2,15 +2,6 @@ const input = document.querySelector('#validation-input');
 input.addEventListener('blur', () => {
     const inputlength = input.value.length;
     const datalength = Number(input.dataset.length);
-
-    if (inputlength === datalength) {
-        if (input.classList.contains('invalid')) {
-         input.classList.replace('invalid', 'valid');   
-        } else {input.classList.add('valid')}
-        return;
-    } else {
-        if (input.classList.contains('valid')) {
-            input.classList.replace('valid', 'invalid');
-        } else {input.classList.add('invalid')}
-    }
+    return inputlength === datalength ? input.className = 'valid' : input.className = 'invalid'
+    
 })
